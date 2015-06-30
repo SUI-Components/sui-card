76a172bebb94ccd9a31327429759d550fc40bcf0
davecarter <dave74@gmail.com>
2015-06-30 12:44:04 +0200

    added test to render subcomponents

--- test/sui-card-test.js
+++ test/sui-card-test.js
@@ -3,6 +3,8 @@ var React = require('react');
 var TestUtils = require('react/lib/ReactTestUtils');
 var expect = require('expect');
 var Card = require('../src/sui-card');
+var CardMedia = require('../src/sui-card-media');
+var CardContent = require('../src/sui-card-content');
 
 describe('sui-card component test suite', function () {
   it('loads without problems', function () {
@@ -12,5 +14,16 @@ describe('sui-card component test suite', function () {
   it('renders into document', function() {
     var root = TestUtils.renderIntoDocument(<Card />);
     expect(root).toExist();
+  });
+
+  describe('Checking sui-card subcomponents structure', function () {
+    it('renders sui-card-media sub-component correctly', function(){
+      var media = TestUtils.renderIntoDocument(<CardMedia />);
+      expect(media).toExist();
+    });
+    it('renders sui-card-content sub-component correctly', function(){
+      var content = TestUtils.renderIntoDocument(<CardContent />);
+      expect(content).toExist();
+    });
   });
 });
