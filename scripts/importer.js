2fd0f4a4673edfb88e4e027c0abc3eba89f7b980
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-07-11 09:19:51 +0200

    added importer

---
+++ scripts/importer.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function (url, file, done) {
+  if (url[0] === '.') {
+    return done({file: url});
+  } else {
+    done({file: recursiveFind(url)});
+  }
+}
+
+function recursiveFind(url) {
+  const urlTree = url.split(path.sep);
+  urlTree.pop();
+  const folder = path.join(__dirname, '../node_modules', urlTree.join(path.sep));
+
+  if (folder === path.join(path.sep + urlTree.join(path.sep))) {
+    return '';
+  }
+
+  if (!fs.existsSync(folder)) {
+    return recursiveFind(path.join('../', url));
+  } else {
+    return url;
+  }
+}
