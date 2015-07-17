b45015eeb9e9cd932facccf6575be2c4300de67c
Joan Leon <joan.leon@gmail.com>
2015-07-17 13:57:02 +0200

    Updates

---
+++ index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Card} from '../src';
+import '../src/index.scss';
+
+React.render(
+  <Card
+    multimediaUrl={'http://images.inmofactory.com/inmofactory/documents/1/98852/7538658/49226769.jpg/300x225/w_0/'}>
+    <div class='content'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Eius beatae excepturi
+        pariatur ullam temporibus ipsa molestias
+        officia eligendi, recusandae, cupiditate
+        quia sunt id, quaerat culpa sint minus,
+        tempore adipisci accusamus.
+      </p>
+    </div>
+  </Card>,
+  document.getElementById('main'));
