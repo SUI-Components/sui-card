574fb8272205015aea0d8f77d297c897b64fd041
Daniel de la Cruz <daniel.delacruz@scmspain.com>
2015-10-07 12:37:36 +0200

    updated dependencies to react 0.14

--- test/utilities/index.js
+++ test/utilities/index.js
@@ -1,8 +1,16 @@
-import React from 'react/addons';
-const TestUtils = React.addons.TestUtils;
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
 
-export function createComponent(component, props, ...children) {
-  const shallowRenderer = TestUtils.createRenderer();
+export function createComponent(component){
+  return class ChildComponent extends React.Component{
+    render() {
+      return(component)
+    }
+  }
+}
+
+export function shallowRender(component, props, ...children) {
+  const shallowRenderer = ReactTestUtils.createRenderer();
   shallowRenderer.render(React.createElement(component, props, children.length > 1 ? children : children[0]));
   return shallowRenderer.getRenderOutput();
 }
